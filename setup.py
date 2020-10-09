5421fbe194ce7fbc48933292d118aa8b8a146a99
Amit Garu <amitgaru2@gmail.com>
2020-10-10 01:21:55 +0545

    setup dependency fix

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pydot", "imageio"],
+    install_requires=["pydot==1.4.1", "imageio==2.6.1"],
     python_requires='>=3.6',
 )
