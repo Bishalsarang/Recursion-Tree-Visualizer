f35366c0d3697c6e8b0848f8a93e5aaa76cb8e6f
Bishalsarang <bishalsarang@gmail.com>
2020-12-26 11:22:27 +0545

    Update to version 1.0.2 with minor fixes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="recursion-visualiser",
-    version="1.0.1",
+    version="1.0.2",
     author="Bishal Sarangkoti",
     author_email="sarangbishal@gmail.com",
     description="A small python package to visualise recursive function on Python. It draws recursion tree",
